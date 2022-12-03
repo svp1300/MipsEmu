a6d5b347580700bf87a4d7779b5ba500183eed1b
Sarah <dop1300@rit.edu>
2022-12-03 17:49:27 -0500

    Added ALU class and setup C# project.

---
+++ src/MipsEmu/Emulation/Hardware/Alu.cs
@@ -0,0 +1,12 @@
+using MipsEmu;
+
+namespace MipsEmu.Emulation {
+
+    public class Alu {
+
+        public Bits Add(Bits a, Bits b) {
+            return a; // TODO implement
+        }
+    }
+    
+}
