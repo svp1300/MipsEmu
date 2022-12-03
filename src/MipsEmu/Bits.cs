b0fe6c50a50e665b8892030b1d1557ae542a6d0f
Sarah <dop1300@rit.edu>
2022-12-03 17:15:17 -0500

    Added various helper functions.

--- src/MipsEmu/Bits.cs
+++ src/MipsEmu/Bits.cs
@@ -2,13 +2,15 @@
 namespace MipsEmu {
 
     class Bits {
+        public static readonly int WORD_SIZE = 32;
+        public static readonly int HALFWORD_SIZE = 16;
         private bool[] values;
 
         public Bits(int size) {
             values = new bool[size];
         }
 
-        private void storeBits(int offset, bool[] bits) {
+        private void StoreBits(int offset, bool[] bits) {
             if (offset < 0 || offset + bits.Length >= values.Length) {
                 throw new IndexOutOfRangeException("Specified bit(s) are out of the available range.");
             } else {
@@ -18,7 +20,7 @@ namespace MipsEmu {
             }
         }
 
-        private bool[] loadBits(int offset, int size) {
+        private bool[] LoadBits(int offset, int size) {
             if (offset < 0 || offset + size >= values.Length) {
                 throw new IndexOutOfRangeException("Specified bit(s) are out of the available range.");
             } else {
@@ -30,10 +32,30 @@ namespace MipsEmu {
             }
         }
 
-        public int getIntFromRange(int offset, int size) {
+        /// TODO implement
+        public int GetIntFromRange(int offset, int size) {
             return 0;
         }
 
         
+        /// <summary>Create and return a duplicate of the values.</summary>
+        public bool[] GetValues() {
+            return LoadBits(0, GetLength()); // returned duplicated as modifications should be done through store
+        }
+
+        public int GetLength() {
+            return values.Length;
+        }
+
+        public static Bits SignExtend(Bits source, int amount) {
+            var result = new Bits(source.GetLength() + amount);
+            result.StoreBits(0, source.values);
+            return result;
+        }
+
+        public static Bits SignExtend16(Bits source) {
+            return SignExtend(source, 16);
+        }
+
     }
 }
