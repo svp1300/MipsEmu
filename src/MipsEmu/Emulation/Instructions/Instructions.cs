2bfbf0a3695b259fbfcad1041c0beb4df5db8bc0
Sarah <dop1300@rit.edu>
2022-12-03 17:16:53 -0500

    Moved instruction definitions to separate cs files and implemented jr, jalr, and add.

--- src/MipsEmu/Emulation/Instructions/Instructions.cs
+++ src/MipsEmu/Emulation/Instructions/Instructions.cs
@@ -1,14 +1,15 @@
 using MipsEmu.Emulation.Registers;
 using MipsEmu.Emulation.Memory;
+
 namespace MipsEmu.Emulation.Instructions {
 
-    interface Instruction {
+    interface IInstruction {
 
-        public void Run(Hardware hardware, Bits bits);
+        void Run(Hardware hardware, Bits bits);
 
     }
 
-    abstract class ITypeInstruction : Instruction {
+    abstract class InstructionIType : IInstruction {
 
         public void Run(Hardware hardware, Bits bits) {
             var imm = bits.Subset(0, 16);
@@ -24,10 +25,10 @@ namespace MipsEmu.Emulation.Instructions {
             return hardware.alu.Add(rs, Bits.SignExtend16(imm)).GetInt();
         }
 
-        public abstract void Run(Hardware hardware, Bits rsVal, Bits rt, Bits imm);
+        public abstract void Run(Hardware hardware, Bits rsValue, int rt, Bits imm);
     }
 
-    abstract class RTypeInstruction : Instruction {
+    abstract class InstructionRType : IInstruction {
 
 
         public void Run(Hardware hardware, Bits bits) {
@@ -37,48 +38,20 @@ namespace MipsEmu.Emulation.Instructions {
             // Run(hardware, rs, rt, imm);
         }
 
-        public abstract void Run(Hardware hardware, int rs, int rt, int rd);
+        public abstract void Run(Hardware hardware, Bits rsValue, Bits rtValue, int rd);
     }
 
-    // // TODO move
-    // class AddInstruction : RTypeInstruction {
+    // class AddInstruction : InstructionRType {
     //     public override void Run(Hardware hardware, int rs, int rt, int rd) {
     //         Bits sourceValue = hardware.registers.GetValue(rs);
     //         Bits targetValue = hardware.registers.GetValue(rt);
             
-    //         // Bits source = hardware.regis
-            
+        
     //     }
     // }
 
-    class LoadWordInstruction : ITypeInstruction {  
+    
 
-        /// <summary>Retrieve the contents at $rs + imm and store it in $rt.</summary>
-        public override void Run(Hardware hardware, Bits rsValue, int rt, Bits imm) {
-            int address = GetStoreAddress(rs, imm);
-            Bits contents = hardware.memory.LoadBits(address, Bits.WORD_SIZE);
-            hardware.registers.StoreValue(rt, contents);
-        }
-
-    }
-
-    class StoreWordInstruction {
-
-        /// <summary>Store the contents of $rt into $rs + imm.</summary>
-        public override void Run(Hardware hardware, Bits rsValue, int rt, Bits imm) {
-            int address = GetStoreAddress(rs, imm);
-            Bits rtValue = hardware.memory.registers.GetValue(rt);
-            hardware.memory.StoreBits(address, rtValue);
-        }
-    }
-
-    class AddImmediateInstruction {
-
-        /// <summary>Add $rs and imm then store in rt.</summary>
-        public override void Run(Hardware hardware, Bits rsValue, int rt, Bits imm) {
-            Bits sum = hardware.alu.Add(rsValue, imm);
-            hardware.registers.StoreValue(rt, sum);
-        }
-    }
+    
 
 }
