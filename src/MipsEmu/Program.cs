136ff43a2589dae9ef985629399d7908a4be6b88
Sarah P <dop1300@rit.edu>
2022-12-13 21:58:18 -0500

    Testing code

--- src/MipsEmu/Program.cs
+++ src/MipsEmu/Program.cs
@@ -2,9 +2,25 @@
 
 namespace MipsEmu;
 
+using MipsEmu.Assembler;
+
 public sealed class Program {
 
     public static void Main(string[] args) {
+        var text = ".data beep:.asciiz \"beep\".text\nmain:\n\tjr $ra";
+        text = ".data values:.byte 5,4,3, 2 beep: .word 16.text .globl main main: addi $t0, $t0, 43 add $t0, $t0, $t1";
+        // text = ".byte 5,4,3,2";
+        var syntaxAnalyzer = new SyntaxAnalyzer();
+        syntaxAnalyzer.AddTokenForm(TypeIInstructionToken.FORM, (s) => new TypeIInstructionToken(s));
+        syntaxAnalyzer.AddTokenForm(TypeRInstructionToken.FORM, (s) => new TypeRInstructionToken(s));
+
+        syntaxAnalyzer.AddTokenForm(ArgumentlessDirectiveToken.FORM, (s) => new ArgumentlessDirectiveToken(s));
+        syntaxAnalyzer.AddTokenForm(TextArgumentDirectiveToken.FORM, (s) => new TextArgumentDirectiveToken(s));
+        syntaxAnalyzer.AddTokenForm(NumberArgumentDirective.FORM, (s) => new NumberArgumentDirective(s));
+        syntaxAnalyzer.AddTokenForm(LabelToken.FORM, (s) => new LabelToken(s));
+        var assembler = new ProgramAssembler(syntaxAnalyzer);
+        assembler.ParseSection(text);
+
         if (args.Length == 0) {
             Console.WriteLine("At least one parameter is required. (run, debug, assemble)");
         } else {
